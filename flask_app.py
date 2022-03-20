13ee46555db34098c483f62a76a09d44fb1f064c
brettacornish <brettacornish@gmail.com>
2022-03-19 17:51:56 -0700

    some comments added and code cleanup

--- flask_app.py
+++ flask_app.py
@@ -57,48 +57,54 @@ class CompleteOrder(db.Model):
     time = db.Column(db.Float())
 
 
-
+#returns all open orders in a format for the client to parse
 def open_order_fetch(limit=False):
     if limit:
         OpenOrders = OpenOrder.query.order_by(OpenOrder.price).limit(limit)
     else:
         OpenOrders = OpenOrder.query.order_by(OpenOrder.price)
     buy_orders, sell_orders=[],[]
-    for x in OpenOrders:
+    for open_order in OpenOrders:
         cont = False
-        if x.buy_sell == "buy":
+        if open_order.buy_sell == "buy":
+            #if two orders are the same price they are added to that prices total
             for y,z in enumerate(buy_orders):
-                if x.price == buy_orders[y]['price']:
-                    buy_orders[y]['quantity'] += x.quantity
+                if open_order.price == buy_orders[y]['price']:
+                    buy_orders[y]['quantity'] += open_order.quantity
                     cont = True
                     break
             if cont:
                 continue
-            buy_orders.append({"price":x.price, "quantity":x.quantity})
+            buy_orders.append({"price":open_order.price, "quantity":open_order.quantity})
         else:
+            #if two orders are the same price they are added to that prices total
             for y,z in enumerate(sell_orders):
-                if x.price == sell_orders[y]['price']:
-                    sell_orders[y]['quantity'] += x.quantity
+                if open_order.price == sell_orders[y]['price']:
+                    sell_orders[y]['quantity'] += open_order.quantity
                     cont = True
                     break
             if cont:
                 continue
-            sell_orders.insert(0,{"price":x.price, "quantity":x.quantity})
+            sell_orders.insert(0,{"price":open_order.price, "quantity":open_order.quantity})
     return {"buy":buy_orders, "sell":sell_orders, "action":"open_order_update"}
 
 
-
+#processes user order(s) and returns completed orders in a format for the client to handle
 def order_request(orderform_user):
     orders = []
     quantity_balance = int(orderform_user['quantity'])
     
-    
+    #Buy order processing
     if orderform_user["trade_type"] == "Buy":
+        #Verification that user has enough money to purchase requested amount of commodity
         if current_user.usd >= int(orderform_user['price']) * quantity_balance:
             open_orders = OpenOrder.query.filter(OpenOrder.buy_sell == "buy").order_by(OpenOrder.price)
             open_order_count = 0
             for open_order in open_orders:
                 open_order_count += 1
+                
+                #if the requested order price is greater than or equal to the price of the current open order and has same or more order quantity that the open order needs to be completed. 
+                #A complete order is created and the open order is deleted. Account balances are adjusted based off of the open order(s) being closed
                 if int(orderform_user['price']) >= open_order.price and quantity_balance >= open_order.quantity:
                     orders.append(CompleteOrder(commodity=orderform_user['commodity'], 
                                                 price=open_order.price, 
@@ -112,6 +118,8 @@ def order_request(orderform_user):
                     current_user.rice += open_order.quantity
                     db.session.delete(open_order)
                     
+                #else if the requested order price is greater than or equal to the current open order price but the requested order quantity is less than the open order quantity.
+                #A complete order is created and the open order is updated to the new open order amount. Account balances are adjusted based off of the open order(s) being closed.
                 elif int(orderform_user['price']) >= open_order.price and quantity_balance > 0:
                     orders.append(CompleteOrder(commodity=orderform_user['commodity'], 
                                                 price=open_order.price, 
@@ -124,8 +132,10 @@ def order_request(orderform_user):
                     current_user.rice += quantity_balance
                     open_order.quantity -= quantity_balance 
                     quantity_balance = 0
+                    break
                     
                     
+                #if no open orders are available but order quantity is not fulfilled a new open order is created. Account balances are adjusted based off of the open order being created.
             if open_order_count == 0 or quantity_balance > 0:
                 new_open_order = OpenOrder(commodity = orderform_user['commodity'], 
                                       price = orderform_user['price'], 
@@ -140,7 +150,7 @@ def order_request(orderform_user):
                 db.session.add(new_open_order)
                 
                 
-                
+            #commit all new open and completed orders to database. Return data to be updated by the client
             db.session.add_all(orders)
             db.session.commit()
             trades = []
@@ -151,13 +161,16 @@ def order_request(orderform_user):
             return {"action":"error", "message":"Insuffient funds or commodity"}
             
             
-            
+    #sell order processing        
     elif orderform_user["trade_type"] == "Sell":
         if current_user.rice >= quantity_balance:
             open_orders = OpenOrder.query.filter(OpenOrder.buy_sell == "sell").order_by(OpenOrder.price.desc())
             open_order_count = 0
             for open_order in open_orders:
                 open_order_count += 1
+                
+                #if the requested order price is less than or equal to the price of the current open order and has same or more order quantity that the open order needs to be completed. 
+                #A complete order is created and the open order is deleted. Account balances are adjusted based off of the open order(s) being closed
                 if int(orderform_user['price']) <= open_order.price and quantity_balance >= open_order.quantity:
                     orders.append(CompleteOrder(commodity=orderform_user['commodity'], 
                                                 price=open_order.price, 
@@ -170,7 +183,9 @@ def order_request(orderform_user):
                     current_user.rice -= open_order.quantity
                     open_order.user.rice += open_order.quantity
                     db.session.delete(open_order)
-                    
+                
+                #else if the requested order price is greater than or equal to the current open order price but the requested order quantity is less than the open order quantity.
+                #A complete order is created and the open order is updated to the new open order amount. Account balances are adjusted based off of the open order(s) being closed.
                 elif int(orderform_user['price']) <= open_order.price and quantity_balance > 0:
                     orders.append(CompleteOrder(commodity=orderform_user['commodity'], 
                                                 price=open_order.price, 
@@ -182,8 +197,9 @@ def order_request(orderform_user):
                     open_order.user.rice += quantity_balance
                     open_order.quantity -= quantity_balance 
                     quantity_balance = 0
+                    break
                     
-
+            #if no open orders are available but order quantity is not fulfilled a new open order is created. Account balances are adjusted based off of the open order being created.
             if open_order_count == 0 or quantity_balance > 0:
                 new_open_order = OpenOrder(commodity = orderform_user['commodity'], 
                                       price = orderform_user['price'], 
@@ -195,8 +211,9 @@ def order_request(orderform_user):
                 current_user.rice -= quantity_balance                      
                 quantity_balance = 0                      
                 db.session.add(new_open_order)
-                
-            print(quantity_balance)
+            
+            
+            #commit all new open and completed orders to database. Return data to be updated by the client
             db.session.add_all(orders)
             db.session.commit()
             trades = []
@@ -217,10 +234,11 @@ def handleMessage(msg):
 @app.route('/create_account',methods=["GET","POST"])
 def create_account():
     if request.method == "POST":
+        #validates emai
         if not re.match(r"[^@]+@[^@]+\.[^@]+", request.form["email"]):
             return render_template("create_account.html", err = "Not a valid email")
         else:
-            print(request.form)
+            #makes sure form was completed
             empty = False
             for x in request.form:
                 if request.form[x] == "":
@@ -229,14 +247,16 @@ def create_account():
             if empty: 
                 return render_template("create_account.html", err = "All fields required")
             else:
+                #makes sure password and confirm password are the same
                 if request.form["password"] != request.form["c_password"]:
                     return render_template("create_account.html", err = "Passwords did not match")
                 else:
+                    #makes sure user doesn't exist already
                     user = User.query.filter_by(email=request.form['email']).first()
-                    print(user)
-                    if user != None:
+                    if user:
                         return render_template("create_account.html", err = "Email already in use")
                     else:
+                        #creates user, hashes password and logs user in
                         new_user = User(email = request.form['email'], password = generate_password_hash(request.form['password']))
                         db.session.add(new_user)
                         db.session.commit()
@@ -251,21 +271,16 @@ def create_account():
 @app.route("/login", methods=["POST","GET"])
 def login():
     if request.method == "POST":
-        user = None
-        users = User.query.all()
-        for x in users:
-            if x.email == request.form["email"] and check_password_hash(x.password, request.form["password"]):
-                user = x
-        if not user:
-            return render_template("login.html",err = "Invalid Credentials")
-        else:
-            login_user(user)
-            return redirect(url_for("home"))
+        user = User.query.filter_by(email=request.form["email"]).first()
+        if not user or not check_password_hash(user.password, request.form["password"]):
+            return render_template("login.html", err="Invalid Credentials")
+        login_user(user)
+        return redirect(url_for("home"))
+
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     else:
-        if current_user.is_authenticated:
-            return redirect(url_for("home"))
-        else:
-            return render_template("login.html")
+        return render_template("login.html")
     
     
 @app.route('/home', methods=['GET', 'POST']) 
